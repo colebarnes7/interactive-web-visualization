978996444e4f4186d2ac1fb38bc14230ae1187f3
Cole Barnes <colebarnes7@gmail.com>
2022-06-09 14:25:05 -0500

    updated app with function setup

--- Code/static/js/app.js
+++ Code/static/js/app.js
@@ -5,13 +5,53 @@ const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/
 d3.json(url).then(function(data){
   console.log(data);
   let samples = data.samples;
+  console.log(samples);
   
-  let sample_values = [];
-  let otu_ids = [];
-  let otu_labels = [];
-  for (let i = 0; i < samples.length; i++) {
-    sample_values.push(samples[i].sample_values);
-    otu_ids.push(samples[i].otu_ids);
-    otu_labels.push(samples[i].otu_labels);
-  }
+  // function that contains instructions at page load/refresh
+  // function does not run until called
+  function init(){
+    // code that runs once (only on page load or refresh)
+    
+    // this checks that our initial function runs.
+    console.log("The Init() function ran");
+    
+    // create dropdown/select
+    
+    // run functions to generate plots
+    createScatter('940');
+    createBar('940');
+    createSummary('940');
+  };
+
+  // function that runs whenever the dropdown is changed
+  // this function is in the HTML and is called with an input called 'this.value'
+  // that comes from the select element (dropdown)
+  function optionChanged(newID){
+    // code that updates graphics
+    // one way is to recall each function
+    createScatter(newID);
+    createBar(newID);
+    createSummary(newID);
+  };
+  function createScatter(id){
+    // code that makes scatter plot at id='bubble'
+  
+    // checking to see if function is running
+    console.log(`This function generates scatter plot of ${id} `);
+  };
+  function createBar(id){
+    // code that makes bar chart at id='bar'
+    
+    // checking to see if function is running
+    console.log(`This function generates bar chart of ${id} `);
+  };
+  function createSummary(id){
+    // code that makes list, paragraph, text/linebreaks at id='sample-meta'
+    
+    // checking to see if function is running
+    console.log(`This function generates summary info of ${id} `);
+  };
+  // function called, runs init instructions
+  // runs only on load and refresh of browser page
+  init();
 });
