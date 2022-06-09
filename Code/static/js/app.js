e957c85003ea2bb5526913a42e97f44001c9a023
Cole Barnes <colebarnes7@gmail.com>
2022-06-09 15:07:58 -0500

    completed bar chart function

--- Code/static/js/app.js
+++ Code/static/js/app.js
@@ -3,9 +3,11 @@ const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/
 
 // Grabs data from URL, everything will be inside this function
 d3.json(url).then(function(data){
+  //Print data to console and then pull each array into its own variable
   console.log(data);
+  let metadata = data.metadata;
+  let names = data.names;
   let samples = data.samples;
-  console.log(samples);
   
   // function that contains instructions at page load/refresh
   // function does not run until called
@@ -33,24 +35,56 @@ d3.json(url).then(function(data){
     createBar(newID);
     createSummary(newID);
   };
+  
   function createScatter(id){
     // code that makes scatter plot at id='bubble'
   
     // checking to see if function is running
     console.log(`This function generates scatter plot of ${id} `);
   };
+  
+  // Function to create a horizontal bar chart at a specific id
   function createBar(id){
-    // code that makes bar chart at id='bar'
-    
+    for (let i = 0; i < names.length; i++){
+      // finds the index where the current id is
+      if (id == names[i]){
+        // Stores top 10 OTUs at current id
+        let sample_values = samples[i].sample_values.slice(0, 10);
+        let temp = samples[i].otu_ids.slice(0, 10);
+        let otu_ids = temp.map(function (x) {
+          return "OTU " + x;
+        })
+        let otu_labels = samples[i].otu_labels.slice(0, 10);
+        // Sets up the data for horizontal bar chart at current id
+        let data = [{
+        type: 'bar',
+        x: sample_values,
+        y: otu_ids,
+        text: otu_labels,
+        transforms: [{
+          type: 'sort',
+          target: 'x',
+          order: 'ascending'
+        }],
+        orientation: 'h'
+        }]
+        //Creates the bar chart with plotly at id='bar'
+        Plotly.newPlot("bar", data);
+        //Exits the loop
+        break;
+      };
+    };
     // checking to see if function is running
     console.log(`This function generates bar chart of ${id} `);
   };
+  
   function createSummary(id){
     // code that makes list, paragraph, text/linebreaks at id='sample-meta'
     
     // checking to see if function is running
     console.log(`This function generates summary info of ${id} `);
   };
+  
   // function called, runs init instructions
   // runs only on load and refresh of browser page
   init();
